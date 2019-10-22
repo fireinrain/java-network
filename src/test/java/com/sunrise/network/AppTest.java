bf598673302beb0ee33c0db5f614eb14477f85b3
lzhaoyang <575563079@qq.com>
2019-10-22 10:03:29 +0800

    增加junit支持

---
+++ src/test/java/com/sunrise/network/AppTest.java
@@ -0,0 +1,17 @@
+package com.sunrise.network;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @description:
+ * @date: 2019/10/22
+ * @author: lzhaoyang
+ */
+public class AppTest {
+
+    @Test
+    public void testBrowserGoogle() {
+    }
+}
