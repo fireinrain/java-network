963944a3619eae286dea7bafba31e7f10f0e2300
lzhaoyang <575563079@qq.com>
2019-10-27 22:11:19 +0800

    socket客户端套接字

--- src/main/java/com/sunrise/network/studyapi/Demo8.java
+++ src/main/java/com/sunrise/network/studyapi/Demo8.java
@@ -22,14 +22,68 @@ public class Demo8 {
         //testSocketAdress();
         //testUsePorxy();
         //testSocketInformation();
-        testSocketOptions();
+        //testSocketOptions();
+        testSocketExp();
 
 
     }
 
-    //socket 可以调整的一些选项
-    private static void testSocketOptions() {
+    //更多异常的种类
+    private static void testSocketExp() {
+        new BindException();
+        new ConnectException();
+        new NoRouteToHostException();
+        new ProtocolException();
+    }
 
+    //socket 可以调整的一些选项
+    /*
+    TCP_NODELY socket 缓冲,尽可能的快的发包 而不是等缓冲区满了再发包
+    SO_LINGER socket 关闭时，如何处理剩下的数据
+    SO_TIMEOUT socket 读取数据超时
+    SO_RCVBUF 接受数据缓冲区大小
+    SO_SENDBUGF 发送数据缓冲区大小
+    SO_KEEPALIVE 保持连接，防止断开
+    OOBINLINE out of band, 发送紧急数据
+    SO_REUSEADDR 复用端口
+    IP_TOS 服务类型
+
+     */
+    private static void testSocketOptions() {
+        Socket socket = new Socket();
+        try {
+            if (!socket.getTcpNoDelay()) {
+                socket.setTcpNoDelay(true);
+                System.out.println("开启tcp no delay");
+            }
+            if (socket.getSoLinger() == -1) {
+                socket.setSoLinger(true, 240);
+            }
+            if (socket.getSoTimeout() != -1) {
+                socket.setSoTimeout(180 * 1000);
+            }
+            System.out.println("发送缓冲区大小： " + socket.getSendBufferSize() / 1024 + "kb");
+            System.out.println("接受缓冲区大小： " + socket.getReceiveBufferSize());
+
+            if (!socket.getKeepAlive()){
+                socket.setKeepAlive(true);
+            }
+
+            if (!socket.getOOBInline()){
+                socket.setOOBInline(true);
+                //socket.sendUrgentData(1);
+            }
+            if (!socket.getReuseAddress()){
+                socket.setReuseAddress(true);
+            }
+            socket.setTrafficClass(0xB8);
+
+
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     //socket 获取一些信息
@@ -56,7 +110,7 @@ public class Demo8 {
         InetSocketAddress inetSocketAddress1 = new InetSocketAddress("www.baidu.com", 80);
         try {
             //15 s超时
-            socket.connect(inetSocketAddress1,1000*15);
+            socket.connect(inetSocketAddress1, 1000 * 15);
 
             //发送请求数据
             OutputStream outputStream = socket.getOutputStream();
@@ -70,15 +124,15 @@ public class Demo8 {
             BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
             int c;
             StringBuilder result = new StringBuilder();
-            while ((c = bufferedInputStream.read())!= -1){
+            while ((c = bufferedInputStream.read()) != -1) {
                 result.append((char) c);
             }
-            System.out.println("result: "+ result.toString());
+            System.out.println("result: " + result.toString());
 
         } catch (IOException e) {
             e.printStackTrace();
-        }finally {
-            if (socket!=null){
+        } finally {
+            if (socket != null) {
                 try {
                     socket.close();
                 } catch (IOException e) {
@@ -106,15 +160,15 @@ public class Demo8 {
     }
 
     //构造但不连接
-    public static void testConstructNoConnect(){
+    public static void testConstructNoConnect() {
         Socket socket = new Socket();
         InetSocketAddress inetSocketAddress = new InetSocketAddress("www.baidu.com", 80);
         try {
             socket.connect(inetSocketAddress);
         } catch (IOException e) {
             e.printStackTrace();
-        }finally {
-            if (socket!=null){
+        } finally {
+            if (socket != null) {
                 try {
                     socket.close();
                 } catch (IOException e) {
@@ -124,8 +178,9 @@ public class Demo8 {
         }
 
     }
+
     //选择指定的本地接口
-    public static void testLocalNetInterface(){
+    public static void testLocalNetInterface() {
 
         try {
             InetAddress[] allByName = InetAddress.getAllByName("127.0.0.1");
