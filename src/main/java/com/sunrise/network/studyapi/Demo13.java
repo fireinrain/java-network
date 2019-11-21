9ec1b74d42df9a9e20c49d8f1a90ddc6d556d95c
lzhaoyang <575563079@qq.com>
2019-11-22 00:23:35 +0800

    update

--- src/main/java/com/sunrise/network/studyapi/Demo13.java
+++ src/main/java/com/sunrise/network/studyapi/Demo13.java
@@ -2,6 +2,7 @@ package com.sunrise.network.studyapi;
 
 import java.io.IOException;
 import java.net.*;
+import java.time.LocalDateTime;
 
 /**
  * @description: UDP 协议
@@ -11,12 +12,39 @@ import java.net.*;
  */
 public class Demo13 {
     public static void main(String[] args) {
+        Runnable runnable = () -> {
+            Demo13.testUDPServer();
+        };
+        Thread thread = new Thread(runnable);
+        thread.start();
         testUDPClient();
-        testUDPServer();
+        //testUDPServer();
     }
 
     private static void testUDPServer() {
-        
+        Integer port = 8888;
+        try (DatagramSocket datagramSocket = new DatagramSocket(port)) {
+            while (true) {
+                byte[] bytes = new byte[1024];
+                DatagramPacket datagramPacket = new DatagramPacket(bytes, 0, 1024);
+                //阻塞  直到收到udp 数据包
+                datagramSocket.receive(datagramPacket);
+                System.out.println("服务器接收到数据");
+
+                LocalDateTime localDateTime = LocalDateTime.now();
+                byte[] dateBytes = localDateTime.toString().getBytes("UTF-8");
+                DatagramPacket datagramPacket1 = new DatagramPacket(dateBytes, dateBytes.length,datagramPacket.getAddress(),datagramPacket.getPort());
+
+                datagramSocket.send(datagramPacket1);
+                System.out.println("服务器发送数据成功");
+
+
+            }
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private static void testUDPClient() {
@@ -24,12 +52,12 @@ public class Demo13 {
         try {
             datagramSocket = new DatagramSocket(0);
             datagramSocket.setSoTimeout(10_0000);
-            InetAddress inetAddress = InetAddress.getByName("tf.nist.gov");
+            InetAddress inetAddress = InetAddress.getByName("localhost");
             //请求包
-            DatagramPacket request = new DatagramPacket(new byte[1], 1, inetAddress, 37);
+            DatagramPacket request = new DatagramPacket(new byte[1], 1, inetAddress, 8888);
             //构造响应包
             byte[] bytes = new byte[1024];
-            DatagramPacket response = new DatagramPacket(bytes, bytes.length);
+            DatagramPacket response = new DatagramPacket(bytes, 0,bytes.length);
             //发送udp数据
             datagramSocket.send(request);
             //接收udp数据
@@ -46,7 +74,7 @@ public class Demo13 {
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
-            if (datagramSocket!=null){
+            if (datagramSocket != null) {
                 datagramSocket.close();
             }
         }
